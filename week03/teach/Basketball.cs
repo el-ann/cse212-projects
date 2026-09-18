80248fc2613927674c1cdc0c65df503ba2db5137
Elizabeth Ann Adjabeng <ewuradwoaann@gmail.com>
2026-09-18 08:38:50 +0000

    Complete W03 team activity: Unique Letters discussion, DisplaySumPairs, Basketball point summary

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -27,10 +27,27 @@ public class Basketball
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            if (players.ContainsKey(playerId))
+            {
+                players[playerId] += points;
+            }
+            else
+            {
+                players[playerId] = points;
+            }
         }
 
         Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
 
-        var topPlayers = new string[10];
+        var topPlayers = players
+            .OrderByDescending(pair => pair.Value)
+            .Take(10)
+            .ToArray();
+
+        foreach (var player in topPlayers)
+        {
+            Console.WriteLine($"Player ID: {player.Key}, Points: {player.Value}");
+        }
     }
 }
--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -28,6 +28,15 @@
     /// </summary>
     /// <param name="numbers">array of integers</param>
     private static void DisplaySumPairs(int[] numbers) {
-        // TODO Problem 2 - This should print pairs of numbers in the given array
+        var seen = new HashSet<int>();
+
+        foreach (var number in numbers) {
+            var complement = 10 - number;
+            
+            if (seen.Contains(complement)) {
+                Console.WriteLine($"{number} {complement}");
+            }
+            seen.Add(number);
+        }
     }
 }
